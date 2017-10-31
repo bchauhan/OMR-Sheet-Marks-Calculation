32a66680755e409fc9c94759262003e0741d587e
bchauhan <barkhachauhan111@gmail.com>
2017-10-31 16:35:52 +0530

    demo DJango project for voting

---
+++ mysite/OMRCheck/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OmrcheckConfig(AppConfig):
+    name = 'OMRCheck'
---
+++ mysite/polls/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+from .models import Choice, Question
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context={'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def details(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    context={'question': question}
+    return render(request, 'polls/details.html',context )
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+'''
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model=Question
+    template_name = 'polls/details.html'
+
+
+class ResultView(generic.DetailView):
+    model=Question
+    template_name = 'polls/results.html'
+
+
+def vote(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',{'question':question, 'error_message':"You did'nt select a choice!"})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        #selected_choice.name=selected_choice.name+", "+question_id.name1
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
+    return HttpResponse("You are voting on question %s."%question_id)
